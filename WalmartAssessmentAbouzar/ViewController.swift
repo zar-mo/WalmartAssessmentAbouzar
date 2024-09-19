a7e98d97f180494ec2d3f40b2c87997538b97d65
Abouzar <abouzarmoradian1@gmail.com>
2024-09-19 10:52:35 -0400

    Initial Commit

---
+++ WalmartAssessmentAbouzar/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WalmartAssessmentAbouzar
+//
+//  Created by Abouzar Moradian on 9/19/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
